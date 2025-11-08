0b2945c3709ed392a19dfd12fd11794536618a8e
vivek73470 <vivek734706@gmail.com>
2025-11-09 01:00:46 +0530

    config changes

--- config/db.js
+++ config/db.js
@@ -5,11 +5,12 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URL}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('Mongo Db connected Successfully')
+        // await mongoose.connect(`${process.env.MONGO_URL}`, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        // })
+        await mongoose.connect(`${process.env.MONGO_URL}`);
+        console.log("Mongo Db connected Successfully");
     } catch (error) {
         console.error("MongoDB Connection Error:", error.message);
         process.exit(1)
